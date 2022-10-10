269c53318f234557043892107fa7249a175a048e
ShreyanshGitam <112847701+ShreyanshGitam@users.noreply.github.com>
2022-10-10 19:51:44 +0530

    Update QuickSort.c

--- QuickSort.c
+++ QuickSort.c
@@ -1,44 +1,39 @@
 //Quick Sort Program
 #include<stdio.h>
-int part(int a[], int l, int h){
-    int pi = l, i = l, j = h, temp;
-    while(i < j){
-        while(a[i] < a[pi]){
-            i++;
-        }
-        while(a[j] > a[pi]){
-            j--;
-        }
-        if(i < j){
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-        }
-    }
-    temp = a[pi];
-    a[pi] = a[j];
-    a[j] = a[pi];
-    return j;
-}
-void quicksort(int a[], int l, int h){
-    if(l < h){
-        int p = part(a, l, h);
-        quicksort(a, l, p - 1);
-        quicksort(a, p + 1, h);
-    }
-}
-
+void quickSort(int [10],int,int);
 int main(){
-    int a[100], n, i;
-    printf("Enter size of the array: ");
-    scanf("%d", &n);
-    printf("Enter elements \n");
-    for(i = 0; i < n; i++){
-        scanf("%d", &a[i]);
-    }
-    quicksort(a, 0, n - 1);
-    printf("Sorted array:");
-    for(int i = 0; i < n; i++){
-        printf(" %d", a[i]);
+  int list[20],size,i;
+  printf("Enter size of the list: ");
+  scanf("%d",&size);
+  printf("Enter %d integer values: ",size);
+  for(i = 0; i < size; i++)
+    scanf("%d",&list[i]);
+  quickSort(list,0,size-1);
+  printf("List after sorting is: ");
+  for(i = 0; i < size; i++)
+    printf(" %d",list[i]);
+}
+void quickSort(int list[10],int first,int last){
+    int pivot,i,j,temp;
+     if(first < last){
+         pivot = first;
+         i = first;
+         j = last;
+         while(i < j){
+             while(list[i] <= list[pivot] && i < last)
+                 i++;
+             while(list[j] > list[pivot])
+                 j--;
+             if(i <j){
+                  temp = list[i];
+                  list[i] = list[j];
+                  list[j] = temp;
+             }
+         }
+         temp = list[pivot];
+         list[pivot] = list[j];
+         list[j] = temp;
+         quickSort(list,first,j-1);
+         quickSort(list,j+1,last);
     }
 }
